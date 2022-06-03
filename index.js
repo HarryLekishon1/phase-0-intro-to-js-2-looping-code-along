7f18871e6d22e5cf23413c0caa7e929e82e1ab81
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-03 23:19:07 +0300

    working with loops

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+
+const messages = []
+
+function writeCards(names, event) {
+
+    for( let i = 0; i < names.length; i++) {
+
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function countDown(countDown) {
+
+    while (countDown > 0 ) {
+        
+        console.log(--countDown);
+    }
+    
+}
